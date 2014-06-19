04a3f7b469e26747e92089e1223dbf2ec115fb93
kevinj <kevin@rocksolidknowledge.com>
2014-06-19 11:35:16 +0100

    Directives

--- angular/public/app/app.js
+++ angular/public/app/app.js
@@ -1,6 +1,6 @@
 var app = angular.module("app", ["ngRoute", "ngResource"]);
 
-app.config(function($routeProvider){
+app.config(function ($routeProvider) {
     $routeProvider
         .when("/", {
             templateUrl: '/template/lists.html',
@@ -44,20 +44,20 @@ app.service("ListService", ["ListResource", function (List) {
         return List.query();
     };
 
-    this.save = function(list){
+    this.save = function (list) {
 
     };
 
-    this.getList = function(id){
-        return List.get({id:id});
+    this.getList = function (id) {
+        return List.get({id: id});
     }
 }]);
 
-app.controller("ListsController", ["$scope", "ListService", "$location", function($scope, ListService, $location) {
+app.controller("ListsController", ["$scope", "ListService", "$location", function ($scope, ListService, $location) {
 
     $scope.lists = ListService.getLists();
 
-    $scope.openList = function(id){
+    $scope.openList = function (id) {
         $location.path("/list/" + id);
     };
 
@@ -74,7 +74,7 @@ app.factory("ListResource", function ($resource) {
 });
 
 
-app.controller("ListController", ["$scope", "ListService", "$routeParams", "$location", function($scope, ListService, $routeParams, $location) {
+app.controller("ListController", ["$scope", "ListService", "$routeParams", "$location", function ($scope, ListService, $routeParams, $location) {
 
     setInterval(function () {
         $scope.$apply(function () {
@@ -82,7 +82,88 @@ app.controller("ListController", ["$scope", "ListService", "$routeParams", "$loc
         })
     }, 1000);
 
-    $scope.currentDate = new Date()
+    $scope.currentDate = new Date();
     $scope.currentTime = new Date().toLocaleTimeString();
     $scope.list = ListService.getList($routeParams.id);
 }]);
+
+app.directive("kjSimple", function () {
+    return {
+        restrict: "EA",
+        replace: false,
+        template: "<div ng-click='sayHello()'>Hello <span ng-transclude></span></div>",
+        transclude: true,
+        link: function (scope) {
+            scope.sayHello = function () {
+                //alert('Hello');
+            }
+        }
+    }
+});
+
+app.directive("kjDouble", function () {
+    return {
+        restrict: "E",
+        scope: {
+            value: "@"
+        },
+        template: '<div>{{result}}</div>',
+        link: function (scope) {
+            var n = +scope.value;
+            scope.result = n * 2;
+        }
+    }
+});
+
+app.directive("kjLists", function () {
+    return{
+        restrict: "E",
+        scope: {
+            title: "@",
+            lists: "=",
+            open: "&"
+        },
+        template: "<h2>{{ title }}</h2>" +
+            "<ul>" +
+            "            <li ng-repeat='list in lists'>" +
+            "            <span ng-click='open({id: list.id})'>{{ list.title }}</span>" +
+            "            </li>" +
+            "    </ul>",
+        link: function (scope) {
+//            scope.openList = function(id){
+//                scope.$parent.openList(id);
+//            }
+        }
+    }
+});
+
+app.directive("kjBad", function(){
+   return {
+       restrict: "E",
+       template: '<div><input type="text" ng-model="person.name"/></div>',
+       scope: true
+   }
+});
+
+app.controller("badController", function($scope){
+    $scope.person = {
+        name: ""
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
